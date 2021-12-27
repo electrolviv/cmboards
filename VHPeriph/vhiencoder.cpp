bed8658e6766fc63b7a46900f5e9ca314d9af8fd
Viktor Glebov <viktor@electro-lviv.com>
2021-12-27 23:31:56 +0200

    fix : STM32Cube V1.8.4 support added
    Serial : Cube 1.8.4 fix support
    Keypad : optimized
    Encoder : optimized
    Added Doxygen template

--- VHPeriph/vhiencoder.cpp
+++ VHPeriph/vhiencoder.cpp
@@ -1,92 +1,88 @@
 #include "vhiencoder.hpp"
 
-#define BYTE4B2(A,B,C,D) ( (A<<6)|(B<<4)|(C<<2)|(D<<0) )
-
+#define BYTE4B2(A, B, C, D) ((A << 6) | (B << 4) | (C << 2) | (D << 0))
 
 const uint8_t VHIncEncoder::enctbl[] = {
 
-    BYTE4B2( STATE_STABLE  , STATE_DEC     , STATE_INC     , STATE_INVALID   ),
-    BYTE4B2( STATE_INC     , STATE_STABLE  , STATE_INVALID , STATE_DEC       ),
-    BYTE4B2( STATE_DEC     , STATE_INVALID , STATE_STABLE  , STATE_INC       ),
-    BYTE4B2( STATE_INVALID , STATE_INC     , STATE_DEC     , STATE_STABLE    ),
+    BYTE4B2(STATE_STABLE, STATE_DEC, STATE_INC, STATE_INVALID),
+    BYTE4B2(STATE_INC, STATE_STABLE, STATE_INVALID, STATE_DEC),
+    BYTE4B2(STATE_DEC, STATE_INVALID, STATE_STABLE, STATE_INC),
+    BYTE4B2(STATE_INVALID, STATE_INC, STATE_DEC, STATE_STABLE),
 
 };
 
-
-VHIncEncoder::VHIncEncoder(const cfg *pcfg) :
-    _value(0), _dir(false), _encPosOld(0),
-    _ptrconfig((void *)pcfg), _changed(false) { }
-
+VHIncEncoder::VHIncEncoder(const cfg *pcfg)
+    : _value(0), _dir(false), _encPosOld(0), _ptrconfig((void *)pcfg),
+      _changed(false) {}
 
 void VHIncEncoder::begin() {
 
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    cfg * pcfg = (cfg *)_ptrconfig;
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  cfg *pcfg = static_cast<cfg *>(_ptrconfig);
 
-    GPIO_InitStruct.Pin     = pcfg->pina;
-    GPIO_InitStruct.Mode    = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull    = GPIO_NOPULL;
-    GPIO_InitStruct.Speed   = GPIO_SPEED_FREQ_MEDIUM;
-    HAL_GPIO_Init((GPIO_TypeDef *)pcfg->porta, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = pcfg->pina;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init((GPIO_TypeDef *)pcfg->porta, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin     = pcfg->pinb;
-    GPIO_InitStruct.Mode    = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull    = GPIO_NOPULL;
-    GPIO_InitStruct.Speed   = GPIO_SPEED_FREQ_MEDIUM;
-    HAL_GPIO_Init((GPIO_TypeDef *)pcfg->portb, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = pcfg->pinb;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init((GPIO_TypeDef *)pcfg->portb, &GPIO_InitStruct);
 
-    // Wait stable level
-    osDelay(1);
+  // Wait stable level
+  osDelay(1);
 
-    _encPosOld = readpins();
+  _encPosOld = readpins();
 }
 
 void VHIncEncoder::tick() {
 
-    // Read PIN A/B levels
-    uint8_t encPosCur = readpins();
+  // Read PIN A/B levels
+  uint8_t encPosCur = readpins();
 
-    // Return on same pos
-    if(encPosCur == _encPosOld)
-        return;
-
-    // Walt through table
-    uint8_t tblbyte = enctbl[_encPosOld];
-    uint8_t tblval  = ( tblbyte >> ( encPosCur << 1 ) );
-
-    if(!(tblval & 2)) {
-        if(tblval & 1)  { _value--; _dir=false; }
-        else            { _value++; _dir=true;  }
-        _changed = true;
-    }
-
+  // Recalculate on state changed
+  if (encPosCur != _encPosOld) {
+    calcpos(encPosCur);
     _encPosOld = encPosCur;
-
+  }
 }
 
 void VHIncEncoder::end() {
-
+  
 }
 
 uint8_t VHIncEncoder::readpins() {
-    cfg *pcfg = (cfg *)_ptrconfig;
-    GPIO_PinState sa = HAL_GPIO_ReadPin((GPIO_TypeDef *)pcfg->porta, pcfg->pina);
-    GPIO_PinState sb = HAL_GPIO_ReadPin((GPIO_TypeDef *)pcfg->portb, pcfg->pinb);
-    return ((sa == GPIO_PIN_SET) ? 2:0) | ((sb == GPIO_PIN_SET) ? 1:0);
+  cfg *pcfg = static_cast<cfg *>(_ptrconfig);
+  GPIO_PinState sa = HAL_GPIO_ReadPin((GPIO_TypeDef *)pcfg->porta, pcfg->pina);
+  GPIO_PinState sb = HAL_GPIO_ReadPin((GPIO_TypeDef *)pcfg->portb, pcfg->pinb);
+  return ((sa == GPIO_PIN_SET) ? 2 : 0) | ((sb == GPIO_PIN_SET) ? 1 : 0);
 }
 
-bool VHIncEncoder::isChanged() {
-    return _changed;
+void VHIncEncoder::calcpos(uint8_t newpos) {
+
+  // Walk through table
+  uint8_t tblbyte = enctbl[_encPosOld];
+  uint8_t tblval = (tblbyte >> (newpos << 1));
+
+  if (!(tblval & 2)) {
+    if (tblval & 1) {
+      _value--;
+      _dir = false;
+    } else {
+      _value++;
+      _dir = true;
+    }
+    _changed = true;
+  }
 }
 
-void VHIncEncoder::resetevt() {
-    _changed = false;
-}
+bool VHIncEncoder::isChanged() { return _changed; }
 
-int32_t VHIncEncoder::readValue() {
-    return _value;
-}
+void VHIncEncoder::resetevt() { _changed = false; }
 
-bool VHIncEncoder::readDirection() {
-    return _dir;
-}
+int32_t VHIncEncoder::readValue() { return _value; }
+
+bool VHIncEncoder::readDirection() { return _dir; }
--- VHPeriph/vhkeypad4x4.hpp
+++ VHPeriph/vhkeypad4x4.hpp
@@ -4,27 +4,53 @@
 
 #include "vhperiph.hpp"
 
+/**
+ * @brief The Key4x4 class \n
+ * PA7, PA6, PA5, PA4,   PA3, PA2, PA1, PA0
+ */
 class Key4x4 {
 
 public:
+  /**
+   * @brief Default Constructor
+   */
+  Key4x4();
 
-    void        begin();
-    void        tick();
-    void        dump();
-    uint16_t    read();
+  /**
+   * @brief Keypad Initialization
+   */
+  void begin();
+
+  /**
+   * @brief Call periodically to scan rows
+   * ( using internal counter for auto-select )
+   */
+  void tick();
+
+/**
+ * @brief dump hexadecimal value to Serial1 (debug purposes)
+ */
+#ifdef COMMENT_
+  void dump();
+#endif
+
+  /**
+   * @brief Read actual keyboard states
+   * @return bitmask : x16 keys status
+   */
+  uint16_t read();
 
 private:
+  uint16_t _vals;
+  uint8_t _cnt;
 
-    uint16_t    _vals;
-    uint8_t     _cnt;
+  uint8_t readpins();
 
-    uint8_t     readpins();
-
-    void        setping(int pin);
-    void        setpinu(int pin);
-    void        setpind(int pin);
-    void        setpin(int pin, bool state);
-
+  void setping(int pin);
+  void setpinu(int pin);
+  void setpind(int pin);
+  void setpin(int pin, bool state);
 };
 
+/// Global Keypad object
 extern Key4x4 keypad4x4;
